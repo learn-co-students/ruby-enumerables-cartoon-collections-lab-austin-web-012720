0b320372e86633689a374e9e6f227bd831b9abac
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-09 23:37:29 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |item, i|
+    puts "#{i+1}. #{item}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer)
+  planeteer.map! do |n|  
+    n.concat("!")
+    n.capitalize()
+  end
+  planeteer
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+      #binding.pry
+  calls.any? do |n|
+    n.size > 4 
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheeses)# code an argument here #hint: use combo of: detect/find and include?()
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    cheeses.detect { |cheese| cheese_types.include?(cheese) }
 end
+  
