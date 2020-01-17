444a6f09e6e93b0b52536a3ccb7ef563e28db44b
Jacques Hebert <jacques.henri.hebert@gmail.com>
2020-01-17 05:24:48 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arr)
+
+  arr.each_with_index { |name, i| puts (i+1).to_s + "." + name }
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  a = []
+  arr.each {|word| a<< word[0...1].upcase + word[1..-1] + "!"}
+  return a
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  
+  return arr.reduce(false) {|mem, word| mem = (mem || word.length >4)}
+  
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  return arr.find {|word| cheese_types.include?(word)}
 end
